81fb64be842821be335aac3339d6b68ed970fb5f
COCONUTmkII <coconutmkii@gmail.com>
2020-07-18 19:12:38 +0300

    Fixed PlayerCharacterController and CharacterMovementBehaviour.

--- Blitzcrank Adventures/Assets/Scripts/CharacterBehaviours/CharacterMovementBehaviour.cs
+++ Blitzcrank Adventures/Assets/Scripts/CharacterBehaviours/CharacterMovementBehaviour.cs
@@ -1,38 +1,33 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class CharacterMovementBehaviour : MonoBehaviour
+namespace CharacterBehaviours
 {
-    [SerializeField] private float _movementSpeed = 15f;
-    [SerializeField] float _velocityChangeSpeed = 15f;
-    private Vector3 _targetMovementVelocity = Vector3.zero;
-    private Vector3 _currentMovementVelocity;
-    private Rigidbody _rigidBody;
-
-    void Start()
+    public class CharacterMovementBehaviour : MonoBehaviour
     {
-        _rigidBody = GetComponent<Rigidbody>();
-    }
+        [SerializeField] private float movementSpeed = 15f;
+        [SerializeField] private float velocityChangeSpeed = 15f;
+        private Vector3 _targetMovementVelocity = Vector3.zero;
+        private Vector3 _currentMovementVelocity;
+        private Rigidbody _rigidBody;
+
+        void Start()
+        {
+            _rigidBody = GetComponent<Rigidbody>();
+        }
     
-    void Update()
-    {
-        _currentMovementVelocity = Vector3.Lerp(
-        _currentMovementVelocity, _targetMovementVelocity * _movementSpeed, _velocityChangeSpeed * Time.deltaTime);
-    }
-    void FixedUpdate()
-    {
-        // float horizontalMovenent = Input.GetAxis("Horizontal");
-        // float verticalMovenent = Input.GetAxis("Vertical");
+        void Update()
+        {
+            _currentMovementVelocity = Vector3.Lerp(
+                _currentMovementVelocity, _targetMovementVelocity * movementSpeed, velocityChangeSpeed * Time.deltaTime);
+        }
+        void FixedUpdate()
+        {
+            _rigidBody.velocity = new Vector3(_currentMovementVelocity.x, _rigidBody.velocity.y, _currentMovementVelocity.z);
+        }
 
-        // Vector3 movement = new Vector3(horizontalMovenent, 0.0f, verticalMovenent);
-        // rigidBody.AddForce (movement * movementSpeed);
-
-        _rigidBody.velocity = new Vector3(_currentMovementVelocity.x, _rigidBody.velocity.y, _currentMovementVelocity.z);
-    }
-
-    public void ChangeCharacterMovementDirection (Vector3 targetDirection)
-    {
-        _targetMovementVelocity = targetDirection.normalized;
+        public void ChangeCharacterMovementDirection (Vector3 targetDirection)
+        {
+            _targetMovementVelocity = targetDirection.normalized;
+        }
     }
 }
--- Blitzcrank Adventures/Assets/Scripts/Controllers/PlayerCharacterController.cs
+++ Blitzcrank Adventures/Assets/Scripts/Controllers/PlayerCharacterController.cs
@@ -1,39 +1,41 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using CharacterBehaviours;
 using Managers;
 using UnityEngine;
 
-public class PlayerCharacterController : MonoBehaviour
+namespace Controllers
 {
-    private CharacterMovementBehaviour _movementBehaviour = null;
-    void Awake()
+    public class PlayerCharacterController : MonoBehaviour
     {
-        _movementBehaviour = GetComponent<CharacterMovementBehaviour>();
-
-        InputManager.Instance.eventPlayerMovementDirectionChanged += OnPlayerMovementDirectionChanged;
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    private void OnDestroy()
-    {
-        if (InputManager.TryInstance != null)
+        private CharacterMovementBehaviour _movementBehaviour;
+        void Awake()
         {
-            InputManager.TryInstance.eventPlayerMovementDirectionChanged -= OnPlayerMovementDirectionChanged;
+            _movementBehaviour = GetComponent<CharacterMovementBehaviour>();
+
+            InputManager.Instance.eventPlayerMovementDirectionChanged += OnPlayerMovementDirectionChanged;
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
+        // Start is called before the first frame update
+        void Start()
+        {
         
-    }
+        }
 
-    private void OnPlayerMovementDirectionChanged (Vector3 targetDirection)
-    {
-        _movementBehaviour.ChangeCharacterMovementDirection(targetDirection);
+        private void OnDestroy()
+        {
+            if (InputManager.TryInstance != null)
+            {
+                InputManager.TryInstance.eventPlayerMovementDirectionChanged -= OnPlayerMovementDirectionChanged;
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+
+        private void OnPlayerMovementDirectionChanged (Vector3 targetDirection)
+        {
+            _movementBehaviour.ChangeCharacterMovementDirection(targetDirection);
+        }
     }
 }
