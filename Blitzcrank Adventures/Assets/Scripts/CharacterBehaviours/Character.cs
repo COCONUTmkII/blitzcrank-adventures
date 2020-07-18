0568094cec25f9ab9a78b321cc929b48fa09500b
COCONUTmkII <coconutmkii@gmail.com>
2020-07-18 18:42:04 +0300

    Changed character field's.

--- Blitzcrank Adventures/Assets/Scripts/CharacterBehaviours/Character.cs
+++ Blitzcrank Adventures/Assets/Scripts/CharacterBehaviours/Character.cs
@@ -4,31 +4,10 @@ namespace CharacterBehaviours
 {
     public abstract class Character
     {
-        #region BasicCharacterField
-        private bool _isAlive;
-        private float _health;
-        private float _mana;
-        // other fields that indicates to all characters will be placed here
-        #endregion
-
         #region BasicCharacterFieldProperty
-        public bool IsAlive
-        {
-            get => _isAlive;
-            set => _isAlive = value;
-        }
-
-        public float Health
-        {
-            get => _health;
-            set => _health = value;
-        }
-
-        public float Mana
-        {
-            get => _mana;
-            set => _mana = value;
-        }
+        public bool IsAlive { get; set; }
+        public float Health { get; set; }
+        public float Mana { get; set; }
         #endregion
         
         /// <summary>
